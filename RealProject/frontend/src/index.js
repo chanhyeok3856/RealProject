051ef176b747bace930200e77b9c1e45916456b7
jiwan kim <wldhks1004@naver.com>
2024-07-05 19:22:01 +0800

    BEST

--- RealProject/frontend/src/index.js
+++ RealProject/frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap CSS 파일 불러오기
+import App from './App'; // App 컴포넌트 불러오기
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
